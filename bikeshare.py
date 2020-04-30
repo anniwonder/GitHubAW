bb6e67a39b6236c5c647970d9a8d817c84841f2d
Annika <annika.schwarz.co@gmail.com>
2020-04-30 12:35:48 +1000

    Added function to display raw data to user

--- bikeshare.py
+++ bikeshare.py
@@ -163,6 +163,53 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+def display_raw_data(df):
+    '''display raw data on screen'''
+    n=5
+    print(df.head(n))
+    while True:
+        more=input("\nWould you like to see more raw data?  Please eneter 'yes' or 'no'\n").lower()
+        if more =='no':
+            break
+        elif more=='yes':
+            n=n+5
+            print(df.iloc[n-5:n, :])
+        else:
+            print('\nInvalid input')
+
+def main():
+    print("\nHello! Let\'s explore some US bikeshare data!")
+    while True:
+        while True:
+            raw_or_stats=input("\nWould you like to see raw data or stats?   Please enter 'raw' or 'stats'\n").lower()
+            if raw_or_stats=='raw':
+                city, month, day = get_filters()
+                df = load_data(city, month, day)
+                display_raw_data(df)
+                break
+            elif raw_or_stats =='stats':
+                new_path, txt_file, files_saved_to = createfilepath()
+                while True:
+                    all_cities=input("Would you like to stats of all three cities?  Please enter 'yes' or 'no'\n").lower()
+                    if all_cities=='yes':#the user like to view all cities
+                            #calculates various stats of all three cities
+                        calculate_all(new_path,txt_file)
+                        break
+                    elif all_cities=='no':
+                        city, month, day = get_filters()
+                        df = load_data(city, month, day)
+                        single_city(city,df,new_path,txt_file)
+                        break
+                    else:
+                        print('Invalid input.\n')
+                print(files_saved_to)
+                txt_file.close()
+                break
+            else:
+                print('\nInvalid input')
+        restart = input("Would you like to restart? Please enter 'yes' or 'no'.\n").lower()
+        if restart.lower() != 'yes':
+            break
 
 def main():
     while True:
